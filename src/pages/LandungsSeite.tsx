9263cda976acf23ceac717c861c46eedd829f368
hcdiekmann <hcdiekmann@hotmail.com>
2023-08-15 15:09:30 +1200

    adds balancer to title

--- src/pages/LandungsSeite.tsx
+++ src/pages/LandungsSeite.tsx
@@ -1,13 +1,14 @@
 import { Container, Title } from '@mantine/core';
 import { AppShellComponent } from '../Appshell/AppShellComponent';
 import { CarouselComponent } from '../Carousel/CarouselComponent';
+import Balancer from 'react-wrap-balancer';
 
 export const LandungsSeite = (): JSX.Element => {
   return (
     <AppShellComponent>
       <Container mt={50}>
         <Title mb={10} order={1}>
-          Ihr Partner für den MB trac und Unimog
+          <Balancer>Ihr Partner für den MB trac und Unimog</Balancer>
         </Title>
 
         <CarouselComponent />
